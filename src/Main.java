e4a629c31115da957708df3e73015e5c6e8591bd
David Cachaza <davidcachazav@gmail.com>
2022-09-22 18:07:41 +0200

    preguntar nombre y edad

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
+        System.out.println("Dime tu nombre y Edad:");
+        System.out.println("- Nombre: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String nombre = scanner.nextLine();
+        System.out.println("- Edad: ");
+        int edad = scanner.nextInt();
+        System.out.println("Te llamas " + nombre + " y tienes " + edad + " anyos.");
+        
+
     }
 }
