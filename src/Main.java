fc4ac4bfbee53fd71ad07db39796c4a757917c06
David Cachaza <davidcachazav@gmail.com>
2022-09-22 18:57:27 +0200

    creada clase qeu te devuelve la edad del usuario

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println("Año de nacimiento: ");
         int ano = scanner.nextInt();
         System.out.println("Te llamas " + nombre + " y naciste el " + dia + "/" + mes + "/" + ano);
+
+        System.out.println(new calcularEdad().decirEdad(ano, mes, dia));
         
 
     }
